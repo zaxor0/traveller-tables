b5e88a92ce5654a8c51be1020a9400fc94750212
Zachary Watts <zaxor0@gmail.com>
2024-06-17 09:45:37 -0400

    adding more details

--- mythic.py
+++ mythic.py
@@ -23,9 +23,11 @@ def main():
     else:
       being = False
       
+  sceneCount = 0
   chaosFactor=5
   while True:
     clear()
+    sceneCount += 1
     # first test the expected scene, page 63
     print('\n### NEW SCENE ###')
     print('\nNow we need to test the scene against our expectations with a d10 versus the Chaos Factor,',str(chaosFactor))
@@ -48,7 +50,7 @@ def main():
         else:
           adjustmentString = sceneAdjustmentTable[adjustmentRoll]
         print('adjust scene by:',adjustmentString,'\n')
-        input("Enter any key to continue\n> ")
+        input("... Enter any key to continue ...")
           
       # if even, alter interrupt the scene
       if sceneRoll % 2 == 0:
@@ -61,71 +63,74 @@ def main():
           if focusValue >= eventFocusRoll:
             chosenFocus = eventFocusTable[focusValue]
             break
-        print('Event Focus:',chosenFocus,'(Rolled',eventFocusRoll,')')
-        input("Enter any key to continue\n> ")
-    else:
-      print("Rolled greater than the Chaos Factor", str(chaosFactor))
-      print("Play out the scene until there is a question.")
-      sceneActive = True
-      while sceneActive == True:
-        question = str(input("\nWhile in the scene, if you have a question enter 'yes' OR if the scene ends enter 'no'\n> "))
-        clear()
-        print('\n ### \n')
-        if question in yesses:
-          sceneActive = True
-          fateLikeSelect = False
-          while fateLikeSelect == False:
-            print("Ask a yes or no question. Then follow your expectations, what is the likelihood of a yes?")
-            number = 1
+        print('Event Focus:',chosenFocus,'- Rolled',eventFocusRoll)
+        input("... Enter any key to continue ...")
+
+
+    print('\nScene',sceneCount,'BEGIN! Play out the scene until there is a question.')
+    sceneActive = True
+    questionCount = 0
+    while sceneActive == True:
+      question = str(input("\nWhile in the scene, if you have a question enter 'yes' OR if the scene ends enter 'no'\n> "))
+      if question in yesses:
+        questionCount += 1
+        sceneActive = True
+        fateLikeSelect = False
+        while fateLikeSelect == False:
+          print("\nAsk a yes or no question. Then follow your expectations, what is the likelihood of a yes?")
+          number = 1
+          for likeliness in fateChart:
+            print(str(number),likeliness)
+            number += 1
+          playerLikely = int(input("Which likeliness will you select?\n> "))
+          if playerLikely > 0 and playerLikely < 10:
+            selection = 1 
             for likeliness in fateChart:
-              print(str(number),likeliness)
-              number += 1
-            playerLikely = int(input("Which likeliness will you select?\n> "))
-            if playerLikely > 0 and playerLikely < 10:
-              selection = 1 
-              for likeliness in fateChart:
-                if selection == playerLikely:
-                  likelihood = likeliness
-                selection += 1
-              fateLikeSelect = True
-              percentages = sorted(fateChart[likelihood][chaosFactor])
-              exceptionalYes = percentages[0]
-              yesAnswer = percentages[1]
-              exceptionalNo = percentages[2]
-            else:
-              print("not a valid selection, select a number between 1 and 9")
-              fateLikeSelect = False
-          fateRoll = diceRoll(1,100)
-#          print('You have selected',likelihood,'which has 3 numbers determined by the chaos factor, which is',chaosFactor)
-#          print('\n    Execptional Yes     1 to',str(exceptionalYes))
-#          print('        Yes           ',str(exceptionalYes + 1),'to',yesAnswer)
-#          print('        No            ',str(yesAnswer + 1),'to',str(exceptionalNo - 1))
-#          print('    Execptional No    ',exceptionalNo,'to 100')
-#          input('\n Press any key to ROLL!')
-#          print("\nRolling...", str(fateRoll))
-          if fateRoll <= exceptionalYes:
-            fateAnswer = 'exceptional YES!'
-          if fateRoll > exceptionalYes and fateRoll <= yesAnswer:
-            fateAnswer = 'YES!'
-          if fateRoll > yesAnswer and fateRoll < exceptionalNo:
-            fateAnswer = 'NO!'
-          if fateRoll >= exceptionalNo:
-            fateAnswer = 'exceptional NO!'
-          print('the fates say ...',fateAnswer,'... rolled',fateRoll,' on [',exceptionalYes,'|',yesAnswer,'|',exceptionalNo,']')
-        else:
-          sceneActive= False  
-          chaosUpdated = False
-          while chaosUpdated == False:
-            chaosEnsued = input("Were the players mostly in control of the scene? Yes or no\n> ")
-            if chaosEnsued in yesses:
-              chaosFactor -= 1
-              chaosUpdated= True
-            elif chaosEnsued in nos:
-              chaosFactor += 1
-              chaosUpdated= True
-            else:
-              chaosUpdated = False
-          print('chaos factor has been changed to', str(chaosFactor))
+              if selection == playerLikely:
+                likelihood = likeliness
+              selection += 1
+            fateLikeSelect = True
+            percentages = sorted(fateChart[likelihood][chaosFactor])
+            exceptionalYes = percentages[0]
+            yesAnswer = percentages[1]
+            exceptionalNo = percentages[2]
+          else:
+            print("not a valid selection, select a number between 1 and 9")
+            fateLikeSelect = False
+        fateRoll = diceRoll(1,100)
+#        print('You have selected',likelihood,'which has 3 numbers determined by the chaos factor, which is',chaosFactor)
+#        print('\n    Execptional Yes     1 to',str(exceptionalYes))
+#        print('        Yes           ',str(exceptionalYes + 1),'to',yesAnswer)
+#        print('        No            ',str(yesAnswer + 1),'to',str(exceptionalNo - 1))
+#        print('    Execptional No    ',exceptionalNo,'to 100')
+#        input('\n Press any key to ROLL!')
+#        print("\nRolling...", str(fateRoll))
+        if fateRoll <= exceptionalYes:
+          fateAnswer = 'exceptional YES!'
+        if fateRoll > exceptionalYes and fateRoll <= yesAnswer:
+          fateAnswer = 'YES!'
+        if fateRoll > yesAnswer and fateRoll < exceptionalNo:
+          fateAnswer = 'NO!'
+        if fateRoll >= exceptionalNo:
+          fateAnswer = 'exceptional NO!'
+        print('the fates say ...',fateAnswer,'... rolled',fateRoll,' on [',exceptionalYes,'|',yesAnswer,'|',exceptionalNo,']')
+      else:
+        sceneActive= False  
+        chaosUpdated = False
+        while chaosUpdated == False:
+          chaosEnsued = input("Were the players mostly in control of the scene? Yes or no\n> ")
+          if chaosEnsued in yesses:
+            chaosFactor -= 1
+            chaosUpdated= True
+          elif chaosEnsued in nos:
+            chaosFactor += 1
+            chaosUpdated= True
+          else:
+            chaosUpdated = False
+        print('chaos factor has been changed to', str(chaosFactor))
+        print('Scene',sceneCount,'has ended. Questions asked:',questionCount)
+        input("\n ... Press any key to continue to your next scene ...")
+        clear()
 
 def diceRoll(dieCount,dieSides):
   dieTotal = 0
--- mythicTables.py
+++ mythicTables.py
@@ -2,23 +2,23 @@
 
 # Fate Chart page 18
 fateChart = {
-  'certain' : { 1 : {10,50,91}, 2 : {13,65,94}, 3 : {15,75,96}, 4 : {17,85,98},
+  'Certain' : { 1 : {10,50,91}, 2 : {13,65,94}, 3 : {15,75,96}, 4 : {17,85,98},
      5 : {18,90,99},  6 : {19,95,100}, 7 : {20,99,'X'}, 8 : {20,99,'X'}, 9 : {20,99,'X'} },
-  'nearly certain' : { 1 : {7,35,88}, 2 : {10,50,91}, 3 : {13,65,94}, 4 : {15,75,96},
+  'Nearly Certain' : { 1 : {7,35,88}, 2 : {10,50,91}, 3 : {13,65,94}, 4 : {15,75,96},
       5 : {17,85,98}, 6 : {18,90,99}, 7 : {19,95,100}, 8 : {20,99,'X'}, 9 : {20,99,'X'}},
-  'very likely' : { 1 : { 5,25,86}, 2 : {7,35,88}, 3 : {10,50,91}, 4 : {13,65,94},
+  'Very Likely' : { 1 : { 5,25,86}, 2 : {7,35,88}, 3 : {10,50,91}, 4 : {13,65,94},
       5 : {15,75,96}, 6 : {17,85,98}, 7 : {18,90,99}, 8 : {19,95,100}, 9 : {20,99,'X'}},
-  'likely' : { 1 : { 3,15,84}, 2 : {5,25,86}, 3 : {7,35,88}, 4 : {10,50,91},
+  'Likely' : { 1 : { 3,15,84}, 2 : {5,25,86}, 3 : {7,35,88}, 4 : {10,50,91},
       5 : {13,65,94}, 6 : {15,75,96}, 7 : {17,85,98}, 8 : {18,90,99}, 9 : {19,95,100}},
   '50/50' : { 1 : { 2,10,83}, 2 : {3,15,84}, 3 : {5,25,86}, 4 : {7,35,88},
       5 : {10,50,91}, 6 : {13,65,94}, 7 : {15,75,96}, 8 : {17,85,98}, 9 : {18,90,99}},
-  'unlikely' : { 1 : { 1,5,82}, 2 : {2,10,83}, 3 : {3,15,84}, 4 : {5,25,86},
+  'Unlikely' : { 1 : { 1,5,82}, 2 : {2,10,83}, 3 : {3,15,84}, 4 : {5,25,86},
       5 : {7,35,88}, 6 : {10,50,91}, 7 : {13,65,94}, 8 : {15,75,96}, 9 : {17,85,98}},
-  'very unlikely' : { 1 : { 'X',1,81}, 2 : {1,5,82}, 3 : {2,10,83}, 4 : {3,15,84},
+  'Very Unlikely' : { 1 : { 'X',1,81}, 2 : {1,5,82}, 3 : {2,10,83}, 4 : {3,15,84},
       5 : {5,25,86}, 6 : {7,35,88}, 7 : {10,50,91}, 8 : {13,65,94}, 9 : {15,75,96}},
-  'nearly impossible' : { 1 : { 'X',1,81}, 2 : {'X',1,81}, 3 : {1,5,82}, 4 : {2,10,83},
+  'Nearly Impossible' : { 1 : { 'X',1,81}, 2 : {'X',1,81}, 3 : {1,5,82}, 4 : {2,10,83},
       5 : {3,15,84}, 6 : {5,25,86}, 7 : {7,35,88}, 8 : {10,50,91}, 9 : {13,65,94}},
-  'impossible' : { 1 : { 'X',1,81}, 2 : {'X',1,81}, 3 : {'X',1,81}, 4 : {1,5,82},
+  'Impossible' : { 1 : { 'X',1,81}, 2 : {'X',1,81}, 3 : {'X',1,81}, 4 : {1,5,82},
       5 : {2,10,83}, 6 : {3,15,84}, 7 : {5,25,86}, 8 : {7,35,88}, 9 : {10,50,91}}
   }
 
@@ -36,7 +36,7 @@ eventFocusTable = {
 
 # Meaning Tables 47 to 49 
 meaningTables = {
-  'action' : {
+  'Action' : {
     1 : {
       1 : 'Abandon', 2 : 'Accompany', 3 : 'Activate', 4 : 'Agree', 5 : 'Ambush', 6 : 'Arrive', 7 : 'Assist', 8 : 'Attack', 
       9 : 'Attain', 10 : 'Bargain', 11 : 'Befriend', 12 : 'Bestow', 13 : 'Betray', 14 : 'Block', 15 : 'Break', 16 : 'Carry', 
@@ -44,7 +44,8 @@ meaningTables = {
       24 : 'Control', 25 : 'Create', 26 : 'Deceive', 27 : 'Decrease', 28 : 'Defend', 29 : 'Delay', 30 : 'Deny', 31 : 'Depart',
       32 : 'Deposit', 33 : 'Destroy', 34 : 'Dispute', 35 : 'Disrupt', 36 : 'Distrust', 37 : 'Divide', 38 : 'Drop', 
       39 : 'Easy', 40 : 'Energize', 41 : 'Escape', 42 : 'Expose', 43 : 'Fail', 44 : 'Fight', 45 : 'Flee', 46 : 'Free', 
-      47 : 'Guide', 48 : 'Harm', 49 : 'Heal', 50 : 'Hinder', 51 : 'Imitate', 52 : 'Imprison', 53 : 'Increase', 54 : 'Indulge',      55 : 'Inform', 56 : 'Inquire', 57 : 'Inspect', 58 : 'Invade', 59 : 'Leave', 60 : 'Lure', 61 : 'Misuse', 62 : 'Move', 
+      47 : 'Guide', 48 : 'Harm', 49 : 'Heal', 50 : 'Hinder', 51 : 'Imitate', 52 : 'Imprison', 53 : 'Increase', 54 : 'Indulge',
+      55 : 'Inform', 56 : 'Inquire', 57 : 'Inspect', 58 : 'Invade', 59 : 'Leave', 60 : 'Lure', 61 : 'Misuse', 62 : 'Move', 
       63 : 'Neglect', 64 : 'Observe', 65 : 'Open', 66 : 'Oppose', 67 : 'Overthrow', 68 : 'Praise', 69 : 'Proceed', 
       70 : 'Protect', 71 : 'Punish', 72 : 'Pursue', 73 : 'Recruit', 74 : 'Refuse', 75 : 'Release', 76 : 'Relinquish', 
       77 : 'Repair', 78 : 'Repulse', 79 : 'Return', 80 : 'Reward', 81 : 'Ruin', 82 : 'Separate', 83 : 'Start', 84 : 'Stop', 
@@ -57,8 +58,10 @@ meaningTables = {
       8 : 'Benefits', 9 : 'Building', 10 : 'Burden', 11 : 'Bureaucracy', 12 : 'Business', 13 : 'Chaos', 14 : 'Comfort', 
       15 : 'Completion', 16 : 'Conflict', 17 : 'Cooperation', 18 : 'Danger', 19 : 'Defense', 20 : 'Depletion', 
       21 : 'Disadvantage', 22 : 'Distraction', 23 : 'Elements', 24 : 'Emotion', 25 : 'Enemy', 26 : 'Energy', 
-      27 : 'Environment', 28 : 'Expectation', 29 : 'Exterior', 30 : 'Extravagance', 31 : 'Failure', 32 : 'Fame', 33 : 'Fear',       34 : 'Freedom', 35 : 'Friend', 36 : 'Goal', 37 : 'Group', 38 : 'Health', 39 : 'Hindrance', 40 : 'Home', 41 : 'Hope', 
-      42 : 'Idea', 43 : 'Illness', 44 : 'Illusion', 45 : 'Individual', 46 : 'Information', 47 : 'Innocent', 48 : 'Intellect',       49 : 'Interior', 50 : 'Investment', 51 : 'Leadership', 52 : 'Legal', 53 : 'Location', 54 : 'Military', 
+      27 : 'Environment', 28 : 'Expectation', 29 : 'Exterior', 30 : 'Extravagance', 31 : 'Failure', 32 : 'Fame', 33 : 'Fear',
+      34 : 'Freedom', 35 : 'Friend', 36 : 'Goal', 37 : 'Group', 38 : 'Health', 39 : 'Hindrance', 40 : 'Home', 41 : 'Hope', 
+      42 : 'Idea', 43 : 'Illness', 44 : 'Illusion', 45 : 'Individual', 46 : 'Information', 47 : 'Innocent', 48 : 'Intellect',
+      49 : 'Interior', 50 : 'Investment', 51 : 'Leadership', 52 : 'Legal', 53 : 'Location', 54 : 'Military', 
       55 : 'Misfortune', 56 : 'Mundane', 57 : 'Nature', 58 : 'Needs', 59 : 'News', 60 : 'Normal', 61 : 'Object', 
       62 : 'Obscurity', 63 : 'Official', 64 : 'Opposition', 65 : 'Outside', 66 : 'Pain', 67 : 'Path', 68 : 'Peace', 
       69 : 'People', 70 : 'Personal', 71 : 'Physical', 72 : 'Plot', 73 : 'Portal', 74 : 'Possessions', 75 : 'Poverty', 
@@ -106,34 +109,37 @@ meaningTables = {
     },
   'Elements' : {
     'Locations' : {
-        1 : 'Abandoned', 2 : 'Active', 3 : 'Artistic', 4 : 'Atmosphere', 5 : 'Beautiful', 6 : 'Bleak', 7 : 'Bright', 
-        8 : 'Business', 9 : 'Calm', 10 : 'Charming', 11 : 'Clean', 12 : 'Cluttered', 13 : 'Cold', 14 : 'Colorful', 
-        15 : 'Colorless', 16 : 'Confusing', 17 : 'Cramped', 18 : 'Creepy', 19 : 'Crude', 20 : 'Cute', 21 : 'Damaged', 
-        22 : 'Dangerous', 23 : 'Dark', 24 : 'Delightful', 25 : 'Dirty', 26 : 'Domestic', 27 : 'Empty', 28 : 'Enclosed', 
-        29 : 'Enormous', 30 : 'Entrance', 31 : 'Exclusive', 32 : 'Exposed', 33 : 'Extravagant', 34 : 'Familiar', 
-        35 : 'Fancy', 36 : 'Festive', 37 : 'Foreboding', 38 : 'Fortunate', 39 : 'Fragrant', 40 : 'Frantic', 
-        41 : 'Frightening', 42 : 'Full', 43 : 'Harmful', 44 : 'Helpful', 45 : 'Horrible', 46 : 'Important', 
-        47 : 'Impressive', 48 : 'Inactive', 49 : 'Intense', 50 : 'Intriguing', 51 : 'Lively', 52 : 'Lonely', 53 : 'Long', 
-        54 : 'Loud', 55 : 'Meaningful', 56 : 'Messy', 57 : 'Mobile', 58 : 'Modern', 59 : 'Mundane', 60 : 'Mysterious', 
-        61 : 'Natural', 62 : 'New', 63 : 'Occupied', 64 : 'Odd', 65 : 'Official', 66 : 'Old', 67 : 'Open', 68 : 'Peaceful', 
-        69 : 'Personal', 70 : 'Plain', 71 : 'Portal', 72 : 'Protected', 73 : 'Protection', 74 : 'Purposeful', 75 : 'Quiet', 
-        76 : 'Reassuring', 77 : 'Remote', 78 : 'Resourceful', 79 : 'Ruined', 80 : 'Rustic', 81 : 'Safe', 82 : 'Services', 
-        83 : 'Simple', 84 : 'Small', 85 : 'Spacious', 86 : 'Storage', 87 : 'Strange', 88 : 'Stylish', 89 : 'Suspicious', 
-        90 : 'Tall', 91 : 'Threatening', 92 : 'Tranquil', 93 : 'Unexpected', 94 : 'Unpleasant', 95 : 'Unusual', 
-        96 : 'Useful', 97 : 'Warm', 98 : 'Warning', 99 : 'Watery', 100 : 'Welcoming'
+      1 : 'Abandoned', 2 : 'Active', 3 : 'Artistic', 4 : 'Atmosphere', 5 : 'Beautiful', 6 : 'Bleak', 7 : 'Bright', 
+      8 : 'Business', 9 : 'Calm', 10 : 'Charming', 11 : 'Clean', 12 : 'Cluttered', 13 : 'Cold', 14 : 'Colorful', 
+      15 : 'Colorless', 16 : 'Confusing', 17 : 'Cramped', 18 : 'Creepy', 19 : 'Crude', 20 : 'Cute', 21 : 'Damaged', 
+      22 : 'Dangerous', 23 : 'Dark', 24 : 'Delightful', 25 : 'Dirty', 26 : 'Domestic', 27 : 'Empty', 28 : 'Enclosed', 
+      29 : 'Enormous', 30 : 'Entrance', 31 : 'Exclusive', 32 : 'Exposed', 33 : 'Extravagant', 34 : 'Familiar', 
+      35 : 'Fancy', 36 : 'Festive', 37 : 'Foreboding', 38 : 'Fortunate', 39 : 'Fragrant', 40 : 'Frantic', 
+      41 : 'Frightening', 42 : 'Full', 43 : 'Harmful', 44 : 'Helpful', 45 : 'Horrible', 46 : 'Important', 
+      47 : 'Impressive', 48 : 'Inactive', 49 : 'Intense', 50 : 'Intriguing', 51 : 'Lively', 52 : 'Lonely', 53 : 'Long', 
+      54 : 'Loud', 55 : 'Meaningful', 56 : 'Messy', 57 : 'Mobile', 58 : 'Modern', 59 : 'Mundane', 60 : 'Mysterious', 
+      61 : 'Natural', 62 : 'New', 63 : 'Occupied', 64 : 'Odd', 65 : 'Official', 66 : 'Old', 67 : 'Open', 68 : 'Peaceful', 
+      69 : 'Personal', 70 : 'Plain', 71 : 'Portal', 72 : 'Protected', 73 : 'Protection', 74 : 'Purposeful', 75 : 'Quiet', 
+      76 : 'Reassuring', 77 : 'Remote', 78 : 'Resourceful', 79 : 'Ruined', 80 : 'Rustic', 81 : 'Safe', 82 : 'Services', 
+      83 : 'Simple', 84 : 'Small', 85 : 'Spacious', 86 : 'Storage', 87 : 'Strange', 88 : 'Stylish', 89 : 'Suspicious', 
+      90 : 'Tall', 91 : 'Threatening', 92 : 'Tranquil', 93 : 'Unexpected', 94 : 'Unpleasant', 95 : 'Unusual', 
+      96 : 'Useful', 97 : 'Warm', 98 : 'Warning', 99 : 'Watery', 100 : 'Welcoming'
       },
     'Characters' : {
       1 : 'Accompanied', 2 : 'Active', 3 : 'Aggressive', 4 : 'Ambush', 5 : 'Animal', 6 : 'Anxious', 7 : 'Armed', 
       8 : 'Beautiful', 9 : 'Bold', 10 : 'Busy', 11 : 'Calm', 12 : 'Careless', 13 : 'Casual', 14 : 'Cautious', 15 : 'Classy', 
       16 : 'Colorful', 17 : 'Combative', 18 : 'Crazy', 19 : 'Creepy', 20 : 'Curious', 21 : 'Dangerous', 22 : 'Deceitful', 
-      23 : 'Defeated', 24 : 'Defiant', 25 : 'Delightful', 26 : 'Emotional', 27 : 'Energetic', 28 : 'Equipped', 29 : 'Excited',      30 : 'Expected', 31 : 'Familiar', 32 : 'Fast', 33 : 'Feeble', 34 : 'Feminine', 35 : 'Ferocious', 36 : 'Foe', 
-      37 : 'Foolish', 38 : 'Fortunate', 39 : 'Fragrant', 40 : 'Frantic', 41 : 'Friend', 42 : 'Frightened', 43 : 'Frightening',      44 : 'Generous', 45 : 'Glad', 46 : 'Happy', 47 : 'Harmful', 48 : 'Helpful', 49 : 'Helpless', 50 : 'Hurt', 
+      23 : 'Defeated', 24 : 'Defiant', 25 : 'Delightful', 26 : 'Emotional', 27 : 'Energetic', 28 : 'Equipped', 29 : 'Excited',
+      30 : 'Expected', 31 : 'Familiar', 32 : 'Fast', 33 : 'Feeble', 34 : 'Feminine', 35 : 'Ferocious', 36 : 'Foe', 
+      37 : 'Foolish', 38 : 'Fortunate', 39 : 'Fragrant', 40 : 'Frantic', 41 : 'Friend', 42 : 'Frightened', 43 : 'Frightening',
+      44 : 'Generous', 45 : 'Glad', 46 : 'Happy', 47 : 'Harmful', 48 : 'Helpful', 49 : 'Helpless', 50 : 'Hurt', 
       51 : 'Important', 52 : 'Inactive', 53 : 'Influential', 54 : 'Innocent', 55 : 'Intense', 56 : 'Knowledgeable', 
       57 : 'Large', 58 : 'Lonely', 59 : 'Loud', 60 : 'Loyal', 61 : 'Masculine', 62 : 'Mighty', 63 : 'Miserable', 
       64 : 'Multiple', 65 : 'Mundane', 66 : 'Mysterious', 67 : 'Natural', 68 : 'Odd', 69 : 'Official', 70 : 'Old', 
       71 : 'Passive', 72 : 'Peaceful', 73 : 'Playful', 74 : 'Powerful', 75 : 'Professional', 76 : 'Protected', 
       77 : 'Protecting', 78 : 'Questioning', 79 : 'Quiet', 80 : 'Reassuring', 81 : 'Resourceful', 82 : 'Seeking', 
-      83 : 'Skilled', 84 : 'Slow', 85 : 'Small', 86 : 'Stealthy', 87 : 'Strange', 88 : 'Strong', 89 : 'Tall', 90 : 'Thieving',      91 : 'Threatening', 92 : 'Triumphant', 93 : 'Unexpected', 94 : 'Unnatural', 95 : 'Unusual', 96 : 'Violent', 
+      83 : 'Skilled', 84 : 'Slow', 85 : 'Small', 86 : 'Stealthy', 87 : 'Strange', 88 : 'Strong', 89 : 'Tall', 90 : 'Thieving',
+      91 : 'Threatening', 92 : 'Triumphant', 93 : 'Unexpected', 94 : 'Unnatural', 95 : 'Unusual', 96 : 'Violent', 
       97 : 'Vocal', 98 : 'Weak', 99 : 'Wild', 100 : 'Young'
       },
     'Objects' : {
